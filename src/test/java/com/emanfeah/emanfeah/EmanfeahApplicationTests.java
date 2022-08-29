f3fc28f8ce27751bf8283cef094245f8c89f344b
emanfeah <emanfeah1@gmail.com>
2022-08-29 14:25:59 +0300

    first commit

---
+++ src/test/java/com/emanfeah/emanfeah/EmanfeahApplicationTests.java
@@ -0,0 +1,13 @@
+package com.emanfeah.emanfeah;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmanfeahApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
